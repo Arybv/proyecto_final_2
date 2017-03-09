9e1e2888c10fd5abaa322bfa83406e6ad57cc112
Arantza Burga <arybv18@gmail.com>
2017-03-09 10:15:48 -0500

    Subir

--- assets/js/app.js
+++ assets/js/app.js
@@ -40,3 +40,77 @@ function puntos(){
     }
   }
 }
+//
+function alumnaLaboratoria(){
+		var datos =
+        [ {
+           nombre: "Meche",
+           apellido: "Zubieta",
+           promocion: "1era. promocion",
+           estado: "Trabajando" },
+
+           nombre: "Arabela",
+           apellido: "Rojas",
+           promocion: "2da. promocion",
+           estado: "Trabajando" },
+
+           nombre: "María",
+           apellido: "Rosán",
+           promocion: "2da. promocion",
+           estado: "Trabajando" },
+
+           nombre: "Guadalupe",
+           apellido: "Racio",
+           promocion: "3era. promocion",
+           estado: "Trabajando" },
+
+           nombre: "Sara",
+           apellido: "Casa",
+           promocion: "3era. promocion",
+           estado: "Trabajando" },
+
+           nombre: "Daniella",
+           apellido: "Durán",
+           promocion: "4ta. promocion",
+           estado: "Trabajando" },
+
+           nombre: "Michelle",
+           apellido: "Seguil",
+           promocion: "5ta. promocion",
+           estado: "Trabajando" },
+
+           nombre: "Diana",
+           apellido: "Navarro",
+           promocion: "5ta. promocion",
+           estado: "Trabajando" }];
+
+var dato = "<ul>";
+  datos.forEach(function(elemento){
+    dato += "<div> Nombre: " + elemento.nombre + "<br>" +
+       			"Apellido: "+ elemento.apellido + "<br>"+
+            "Promoción: "+ elemento.promocion + "<br>" +
+            "Estado: "+ elemento.estado + "<br><br></div>";
+       		                        });
+    dato += "</ul>";
+		  document.write(dato);
+  }
+alumnaLaboratoria();
+
+//
+function myFunction() {
+	document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+	if (!event.target.matches('.dropbtn')) {
+
+		var dropdowns = document.getElementsByClassName("dropdown-content");
+		var i;
+		for (i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+			}
+		}
+	}
+}
